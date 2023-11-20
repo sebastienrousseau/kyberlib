bd60663d2ccee7c7e3fcf19cca67fee11191307e
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-11-20 19:08:27 +0000

    fix(kyberlib): :ambulance: disabling criterion benchmark for now

---
+++ benches/bench.rs
@@ -0,0 +1,20 @@
+// Copyright © 2023 KyberLib. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! This crate is responsible for benchmarking various components of the application.
+
+// extern crate criterion;
+// use criterion::{criterion_group, criterion_main};
+
+// /// This is a module for benchmarking API operations.
+// mod api;
+
+// Entry point for all benchmarks.
+// criterion_group!(
+//     name = benches;
+//     config = Criterion::default()
+//     targets = api::keypair, api::encap, api::decap, api::decap_fail, api::decode_hex
+// );
+
+// // Run benchmarks
+// criterion_main!(benches);
